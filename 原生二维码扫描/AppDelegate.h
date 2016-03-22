a1d52419402cd78213de6b1b3244d0a438d51f33
caixiaofan1 <378467584@qq.com>
2016-03-22 16:20:08 +0800

    Initial Commit

---
+++ 原生二维码扫描/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  原生二维码扫描
+//
+//  Created by tcyf-2 on 16/3/22.
+//  Copyright © 2016年 tcyf-2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 原生二维码扫描/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  原生二维码扫描
+//
+//  Created by tcyf-2 on 16/3/22.
+//  Copyright © 2016年 tcyf-2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
